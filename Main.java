504438cd378cc978c48438e0e7f22554c8f4c0ec
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-04-17 21:20:04 +0000

    commit 7

--- Main.java
+++ Main.java
@@ -3,17 +3,17 @@ public class Main
   public static void main(String [] args)
   {
     // Student(String name, int age, String gender, String idNum, double gpa)
-    Person me = new Student("H", 3, "M", "2584", 4.0);
+    Person me = new Student("jonah", 105, "Gender Fluid", "2584", 0.2);
     
     System.out.println("Hi, " + me.getName() + ". What's your GPA?");
     System.out.println("My GPA is: " + ((Student)me).getGPA());
 
-    Teacher you = new Teacher("C", 30, "F");
+    Teacher you = new Teacher("John Pork", 30, "F");
     System.out.println("My teacher is: " + you.getName());
     System.out.println(you);
 
-    CollegeStudent jonah = new CollegeStudent("jonah Junior", 16, "M", "UCB123", 4.0, 1, "I don't know");
-    System.out.println("My college friend is: " + ima.getName());
+    CollegeStudent jonah = new CollegeStudent("Tim Cheese", 16, "Non-binary", "321044210", 0.1, 3, "I don't know");
+    System.out.println("My college friend is: " + jonah.getName());
     System.out.println(jonah);
   }
 }
