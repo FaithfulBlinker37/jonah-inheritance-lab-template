a5822a562a11d6b7a0bd17ed0c943323c4ea048a
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-04-17 20:50:24 +0000

    commit 2

--- Teacher.java
+++ Teacher.java
@@ -8,9 +8,26 @@ public class Teacher extends Person{
     public Teacher(String name, int age, String gender)
     {
         super(name, age, gender);
+        subject = subject;
+        salary = salary;
     }
 
     // TODO Add methods
+    public String getSubject() {
+        return subject;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSubject(String newSubject) {
+        subject = newSubject;
+    }
+
+    public void setSalary(double newSalary) {
+        salary = newSalary;
+    }
 
     public String toString(){
         // TODO: improve this.
