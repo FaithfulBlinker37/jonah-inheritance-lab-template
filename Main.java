6820764ab3c744080976491d072a9d6d67a176bf
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-04-17 21:21:06 +0000

    commit 8

--- Main.java
+++ Main.java
@@ -8,11 +8,11 @@ public class Main
     System.out.println("Hi, " + me.getName() + ". What's your GPA?");
     System.out.println("My GPA is: " + ((Student)me).getGPA());
 
-    Teacher you = new Teacher("John Pork", 30, "F");
+    Teacher you = new Teacher("John Pork", 40, "Xe/Xer");
     System.out.println("My teacher is: " + you.getName());
     System.out.println(you);
 
-    CollegeStudent jonah = new CollegeStudent("Tim Cheese", 16, "Non-binary", "321044210", 0.1, 3, "I don't know");
+    CollegeStudent jonah = new CollegeStudent("Tim Cheese", 47, "Non-binary", "321044210", 0.1, 3, "I don't know");
     System.out.println("My college friend is: " + jonah.getName());
     System.out.println(jonah);
   }
